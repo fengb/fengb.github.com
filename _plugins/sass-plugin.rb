8aafb76659331c89338b045552e39e93af2116f8
fengb <benjamin.feng@gmail.com>
2012-09-10 23:53:38 -0500

    Added configuration to SASS plugin

--- _plugins/sass-plugin.rb
+++ _plugins/sass-plugin.rb
@@ -18,10 +18,19 @@ module Jekyll
       ".css"
     end
 
+    def sass_config
+      if @sass_config.nil?
+        @sass_config = {:syntax => :scss, :load_paths => ["."]}
+        (@config['sass'] || {}).each do |key, value|
+          @sass_config[key.to_sym] = value
+        end
+      end
+      @sass_config
+    end
+
     def convert(content)
       begin
-        puts "Performing Sass Conversion."
-        engine = Sass::Engine.new(content, :syntax => :scss, :load_paths => ["."])
+        engine = Sass::Engine.new(content, sass_config)
         engine.render
       rescue StandardError => e
         puts "!!! SASS Error: " + e.message
