0fe85b3b1341ce01fc8bdb126e186000f9a9e90b
fengb <benjamin.feng@gmail.com>
2012-09-13 21:13:44 -0500

    Minor fixes

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
 (function($) {
-  $('span.email').each(function(i, e) {
-    var email = $(e).text().replace(/ \(dot\) /g, '.').replace(/ \(at\) /, '@');
-    $(e).replaceWith('<a class="email" href="mailto:' + email + '">' + email + '</a>');
-  });
 })(jQuery)
