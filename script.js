7d36622b68987e3bcdd076c31e18cc7e7a01c7bf
fengb <benjamin.feng@gmail.com>
2012-09-14 18:51:12 -0500

    Added basic AJAX call with evil parsing

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 (function($) {
+  $.ajax('/contact.html', {
+    'success':function(data) {
+      /* FIXME: regex should not parse HTML! */
+      $('<aside id="contact" />').insertBefore('footer#copy')
+                                 .append(data.replace(/[\s\S]*<article.*>([\s\S]*)<\/article>[\s\S]*/, '$1'));
+    }
+  });
 })(jQuery)
