45e5c9d8466b78dd9bede22375e012695e50bc88
fengb <benjamin.feng@gmail.com>
2012-09-14 19:16:50 -0500

    Added fancy contact list toggling

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 (function($) {
-  $.ajax('/contact.html', {
-    'success':function(data) {
-      /* FIXME: regex should not parse HTML! */
-      $('<aside id="contact" />').insertBefore('footer#copy')
-                                 .append(data.replace(/[\s\S]*<article.*>([\s\S]*)<\/article>[\s\S]*/, '$1'));
-    }
-  });
+  if($('article#main.contact').length === 0) {
+    var $contact = $('<aside id="contact" />').insertBefore('footer#copy');
+    $.ajax('/contact.html', {
+      'success':function(data) {
+        /* FIXME: regex should not parse HTML! */
+        $contact.html(data.replace(/[\s\S]*<article.*>([\s\S]*)<\/article>[\s\S]*/, '$1'));
+      }
+    });
+    $('nav#site .contact').remove();
+    $('<a href="#">C</span>').appendTo('nav#site').click(function(evt) {
+      var $toggle = $(evt.target);
+      if($toggle.hasClass('active')) {
+        $toggle.removeClass('active');
+        $contact.removeClass('active');
+      } else {
+        $toggle.addClass('active');
+        $contact.addClass('active');
+      }
+      return false;
+    });
+  }
 })(jQuery)
