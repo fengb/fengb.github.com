68b5ae18131df01f8ce3d7f7adce62cda98c00eb
fengb <benjamin.feng@gmail.com>
2012-09-14 23:39:15 -0500

    Fixed javascript element popping

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 (function($) {
   if($('article#main.contact').length === 0) {
-    var $contact = $('<aside id="contact" />').insertBefore('footer#copy');
+    var $contact = $('aside#contact');
     $.ajax('/contact.html', {
       'success':function(data) {
         /* FIXME: regex should not parse HTML! */
         $contact.html(data.replace(/[\s\S]*<article.*>([\s\S]*)<\/article>[\s\S]*/, '$1'));
       }
     });
-    $('nav#site .contact').remove();
-    var $toggle = $('<a class="toggle-contact" href="#">C</span>').appendTo('nav#site');
+    var $toggle = $('nav#site a.toggle-contact');
     $toggle.click(function(evt) {
       if($toggle.hasClass('active')) {
         $toggle.removeClass('active');
