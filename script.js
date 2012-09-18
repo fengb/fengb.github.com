d975cc658ede4048bed18981f34bd767c2e05135
fengb <benjamin.feng@gmail.com>
2012-09-18 03:10:52 -0500

    Prepare for mutiny!

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 (function($) {
-  if($('article#main.contact').length === 0) {
-    var $contact = $('<nav id="contact"/>').insertAfter('article#main');
-    $.get('/contact.html', function(data) {
-        /* FIXME: regex should not parse HTML! */
-        $contact.html(data.replace(/^[\s\S]*?<article[^>]*>\s*([\s\S]*)\s*<\/article>[\s\S]*?$/, '$1'));
-    });
-    var $toggle = $('nav#site .contact a');
-    $toggle.click(function() {
-      if($toggle.hasClass('active')) {
-        $toggle.removeClass('active');
-        $contact.removeClass('active');
-      } else {
-        $toggle.addClass('active');
-        $contact.addClass('active');
-      }
-      return false;
-    });
-  }
+  var $contact = $('<nav id="contact"/>').insertAfter('article#main');
+  $.get('/contact.html', function(data) {
+      /* FIXME: regex should not parse HTML! */
+      $contact.html(data.replace(/^[\s\S]*?<article[^>]*>\s*([\s\S]*)\s*<\/article>[\s\S]*?$/, '$1'));
+  });
+
+  var $toggle = $('[data-mutiny]');
+  var $target = $($toggle.data('mutiny')['toggler']);
+  $toggle.click(function() {
+    if($toggle.hasClass('active')) {
+      $toggle.removeClass('active').addClass('inactive');
+      $target.removeClass('active').addClass('inactive');
+    } else {
+      $toggle.addClass('active').removeClass('inactive');
+      $target.addClass('active').removeClass('inactive');
+    }
+    return false;
+  });
 })(jQuery)
