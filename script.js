e8e1e639d556eb577b8f850b9cfe7445a6603e65
fengb <benjamin.feng@gmail.com>
2012-09-18 02:13:31 -0500

    Almost pointless refactor

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 (function($) {
   if($('article#main.contact').length === 0) {
     var $contact = $('<nav id="contact"/>').insertAfter('article#main');
-    $.ajax('/contact.html', {
-      'success':function(data) {
+    $.get('/contact.html', function(data) {
         /* FIXME: regex should not parse HTML! */
         $contact.html(data.replace(/^[\s\S]*?<article[^>]*>\s*([\s\S]*)\s*<\/article>[\s\S]*?$/, '$1'));
-      }
     });
     var $toggle = $('nav#site .contact a');
     $toggle.click(function() {
