8f6a5b2bce775aac52ad6cf6624856c49981d187
fengb <benjamin.feng@gmail.com>
2012-09-19 23:28:57 -0500

    Added mutiny.js.  Yarrrrgh

--- script.js
+++ script.js
@@ -5,16 +5,5 @@
       $contact.html(data.replace(/^[\s\S]*?<article[^>]*>\s*([\s\S]*)\s*<\/article>[\s\S]*?$/, '$1'));
   });
 
-  var $toggle = $('[data-mutiny]');
-  var $target = $($toggle.data('mutiny')['toggler']);
-  $toggle.click(function() {
-    if($toggle.hasClass('active')) {
-      $toggle.removeClass('active').addClass('inactive');
-      $target.removeClass('active').addClass('inactive');
-    } else {
-      $toggle.addClass('active').removeClass('inactive');
-      $target.addClass('active').removeClass('inactive');
-    }
-    return false;
-  });
+  Mutiny.init();
 })(jQuery)
