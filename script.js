fe313d7a8fe30dad9464a130041c5a505a509d6e
fengb <benjamin.feng@gmail.com>
2012-09-25 22:42:44 -0500

    Changed contact block to be aside instead of nav

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function($) {
-  var $contact = $('<nav id="contact"/>').insertAfter('article#main');
+  var $contact = $('<aside id="contact"/>').insertAfter('article#main');
   $.get('/contact.html', function(data) {
       /* FIXME: regex should not parse HTML! */
       $contact.html(data.replace(/^[\s\S]*?<article[^>]*>\s*([\s\S]*)\s*<\/article>[\s\S]*?$/, '$1'));
