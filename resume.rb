d122f247d41bbc94fd8f5a5d57473b6449e04140
fengb <benjamin.feng@gmail.com>
2013-01-29 22:38:27 -0600

    Added more employment entries

--- resume.rb
+++ resume.rb
@@ -23,7 +23,9 @@ module Fengb
     def entry(options)
       text "#{options[:title]} - #{options[:start_date]} to #{options[:end_date]}"
       text "<link href='#{options[:url]}'>#{options[:location]}</link>", :inline_format => true
-      text options[:misc]
+      if options[:misc]
+        text options[:misc]
+      end
     end
   end
 end
@@ -38,6 +40,32 @@ Fengb::Resume.generate('resume.pdf') do
     skill 'Frameworks', 'Django, jQuery, Ruby on Rails'
     skill 'Versioning', 'Git, Mercurial, Perforce, Subversion'
     skill 'Software', 'Adobe Photoshop, Keynote, vi'
+  end
+
+  section 'Employment' do
+    entry(:title => 'Sr. Software Engineer II',
+          :location => 'Enova Financial',
+          :url => 'http://www.enova.com/',
+          :start_date => Date.new(2009, 10, 26),
+          :end_date => 'present')
+
+    entry(:title => 'Chief Technical Officer',
+          :location => 'Crusader Storm (defunct)',
+          :url => 'http://web.archive.org/web/20100623115357/http://www.crusaderstorm.com/',
+          :start_date => Date.new(2010, 5, 7),
+          :end_date => Date.new(2011, 5, 7))
+
+    entry(:title => 'Software Engineer',
+          :location => 'Business Logic Corporation',
+          :url => 'http://businesslogic.com/',
+          :start_date => Date.new(2007, 6, 4),
+          :end_date => Date.new(2009, 8, 4))
+
+    entry(:title => 'Computer Engineering Intern',
+          :location => 'Rose-Hulman Ventures',
+          :url => 'http://rhventures.org/',
+          :start_date => Date.new(2005, 6, 6),
+          :end_date => Date.new(2007, 3, 16))
   end
 
   section 'Education' do
