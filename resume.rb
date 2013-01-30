7e1824530e20ce1a94c2e47b0422964773011c52
fengb <benjamin.feng@gmail.com>
2013-01-29 22:18:50 -0600

    Better skill generation

--- resume.rb
+++ resume.rb
@@ -3,11 +3,20 @@ require 'prawn'
 
 module Fengb
   class Resume < Prawn::Document
-    def section(title, body)
-      draw_text title, :at => [0, cursor-10], :size => 14
+    def section(title, body=nil, &block)
+      text_box title, :at => [0, cursor], :size => 14
       bounding_box([100, cursor], :width => margin_box.width - 100) do
-        text body
+        if body.nil?
+          yield
+        else
+          text body
+        end
       end
+    end
+
+    def skill(category, values)
+      text_box category << ':', :at => [0, cursor]
+      text values, :indent_paragraphs => 80
     end
   end
 end
@@ -17,12 +26,16 @@ Fengb::Resume.generate('resume.pdf') do
 
   section 'Objective', 'To solve intriguing and intricate problems with emphasis on usability, maintainability, and correctness.'
 
-  section 'Skills', "Languages: C, Javascript, Objective-C, Python, Ruby, SQL\n"+
-                    "Frameworks: Django, jQuery, Ruby on Rails\n"+
-                    "Versioning: Git, Mercurial, Perforce, Subversion\n"+
-                    "Software: Adobe Photoshop, Keynote, vi"
+  section 'Skills' do
+    skill 'Languages', 'C, Javascript, Objective-C, Python, Ruby, SQL'
+    skill 'Frameworks', 'Django, jQuery, Ruby on Rails'
+    skill 'Versioning', 'Git, Mercurial, Perforce, Subversion'
+    skill 'Software', 'Adobe Photoshop, Keynote, vi'
+  end
 
-  section 'Education', "B.S. Computer Engineering - Sep 2003 to May 2007\n"+
-                       "Rose-Hulman Institute of Technology\n"+
-                       "Minor in Computer Science, Economics"
+  section 'Education' do
+    text 'B.S. Computer Engineering - Sep 2003 to May 2007'
+    text 'Rose-Hulman Institute of Technology'
+    text 'Minor in Computer Science, Economics'
+  end
 end
