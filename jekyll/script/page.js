433c7024cd11468e26f5be21e21ee625d92e1461
fengb <benjamin.feng@gmail.com>
2013-06-14 17:03:18 -0500

    Removed image prefetch

--- jekyll/script/page.js
+++ jekyll/script/page.js
@@ -1,11 +1,3 @@
 (function($, undefined) {
   FastClick.attach(document.body);
-
-  /* On document.ready to let browsers report load completion */
-  $(document).ready(function() {
-    $('body#portfolio [href$=png]').each(function(i, e) {
-      var img = new Image();
-      img.src = e.href;
-    });
-  });
 })(jQuery);
