845a1f39263f6cc2e52f9f469f28067bb86150db
fengb <contact@fengb.info>
2014-03-12 09:03:23 -0500

    Made GoogleAnalytics script readable

---
+++ jekyll/script/ga.js
@@ -0,0 +1,16 @@
+!function(window, document, scriptTagName){
+  var insertScript = function(src, newTag, loadedTag){
+    newTag = document.createElement('script')
+    newTag.async = 1
+    newTag.src = src
+    loadedTag = document.getElementsByTagName('script')[0]
+    loadedTag.parentNode.insertBefore(newTag, loadedTag)
+  }
+
+  window.GoogleAnalyticsObject = 'ga'
+  window.ga = {
+    l: +new Date(),
+    q: [['create', 'UA-34997099-2', 'fengb.info'], ['send', 'pageview']]
+  }
+  insertScript('//www.google-analytics.com/analytics.js')
+}(window, document);
