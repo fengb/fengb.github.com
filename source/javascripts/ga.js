9df5663577c7d3d08e2400cef33f3521e6809b9e
fengb <contact@fengb.info>
2014-06-08 23:03:18 -0500

    Cleanup scripts

--- source/javascripts/ga.js
+++ source/javascripts/ga.js
@@ -1,16 +1,13 @@
 !function(window, document){
-  var insertScript = function(src, newTag, loadedTag){
-    newTag = document.createElement('script')
-    newTag.async = 1
-    newTag.src = src
-    loadedTag = document.getElementsByTagName('script')[0]
-    loadedTag.parentNode.insertBefore(newTag, loadedTag)
-  }
-
   window.GoogleAnalyticsObject = 'ga'
   window.ga = {
     l: +new Date(),
     q: [['create', 'UA-34997099-2', 'fengb.info'], ['send', 'pageview']]
   }
-  insertScript('//www.google-analytics.com/analytics.js')
+
+  var newTag = document.createElement('script')
+  newTag.async = 1
+  newTag.src = '//www.google-analytics.com/analytics.js'
+  var loadedTag = document.getElementsByTagName('script')[0]
+  loadedTag.parentNode.insertBefore(newTag, loadedTag)
 }(window, document)
